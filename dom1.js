016755ca37ce5f0a1ea0cd7c4b16c868f5beef82
haidz1q <167000442+haidz1q@users.noreply.github.com>
2025-05-21 22:28:50 +0700

    Add files via upload

---
+++ dom1.js
@@ -0,0 +1,12 @@
+function doiMau(the)
+{
+    the.style.backgroundcolor = the.id
+}
+function tatCa(){
+    var toanBoThe = document.querySelectorAll('div')
+    for(let i = 0 ; i < toanBoThe.length ; i++)
+    {
+        toanBoThe[i].style.backgroundcolor = toanBoThe[i].id
+    }
+
+}
