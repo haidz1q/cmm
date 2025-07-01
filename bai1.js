dce1a17fddc55c2baa744c015db71ba4158f2a13
haidz1q <167000442+haidz1q@users.noreply.github.com>
2025-07-01 23:36:13 +0700

    Add files via upload

---
+++ bai1.js
@@ -0,0 +1,18 @@
+let a = prompt("Nhập số a:")
+let b = prompt("nhập số b:")
+if(a%1 !== 0){
+        console.log("a không phải số nguyên")
+    }
+if(b%1 !== 0){
+        console.log("b không phải số nguyên")
+    }
+
+    
+// Câu 1(2 điểm) Nhập vào từ bàn phím 2 số nguyên a và b bằng hàm promt(), báo
+// lỗi nếu giá trị a và b được nhập vào không phải là số nguyên, sau đó in vào console
+// hình chữ nhật bằng kí tự ‘*’ với a là chiều dài của hình và b là chiều cao
+// VD: a = 3 và b = 4 thì hình sẽ như sau :
+// ***
+// ***
+// ***
+// ***
